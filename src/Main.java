46e9e46d9756c15728589acfb98ac444591ca619
ilyass <ilyassselarabi1999@gmail.com>
2025-10-14 17:26:23 +0100

    math class

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,35 @@
 import java.util.Scanner;
-import java.util.Random ;
+import java.util.InputMismatchException; // Import ajouté
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Random rand = new Random();
+
+        double a;
+        double b;
+        double z;
+        double result;
 
         try {
-            int number ;
-            boolean isHeads;
-            number = rand.nextInt(77,7766)+1 ;
-            isHeads=rand.nextBoolean() ;
+            System.out.print(" Enter the first number: ");
+            a = sc.nextDouble();
+            System.out.print(" Enter the second number: ");
+            b = sc.nextDouble();
 
-            if(isHeads){
-                System.out.println("is true");
-            }
-            else{
-                System.out.println("is false");
-            }
-            System.out.println(number);
+            z = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+//            result=Math.round(z);
+//            result=Math.abs(z);
+//            result=Math.ceil(z);
+//            result=Math.floor(z);
+//            result=Math.max(z,122);
+            result=Math.min(z,12);
+              System.out.println(Math.PI);
+              System.out.println(Math.E);
 
-        } catch (NumberFormatException e) {
+            System.out.println("Result: " + z); // println pour meilleure lisibilité
+            System.out.println("Result: " + result); // println pour meilleure lisibilité
+
+        } catch (InputMismatchException e) { // Exception corrigée
             System.out.println(" Error: Please enter valid numbers!");
         } finally {
             sc.close();
