eee98fefea7ac340d1975cbdc2c7f3b8350e5b8b
ilyass <ilyassselarabi1999@gmail.com>
2025-10-14 09:58:56 +0100

    mad libs game

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,37 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        double width = 0;
-        double height = 0; // Correction: "height" au lieu de "hieght"
-        double area = 0;
+        String adjective1 ;
+        String noun1 ; // Correction: "height" au lieu de "hieght"
+        String adjective2 ;
+        String verb1 ; // Correction: "height" au lieu de "hieght"
+        String adjective3 ;
 
         try {
-            System.out.print("Enter the width: ");
-            width = Double.parseDouble(scanner.nextLine());
+            System.out.print("Enter an adjective (description): ");
+            adjective1 = scanner.nextLine();
 
-            System.out.print("Enter the hieght: ");
-            height = Double.parseDouble(scanner.nextLine());
+            System.out.print("Enter an noun (animal or person): ");
+            noun1 = scanner.nextLine();
 
-            area = (width * height);
+            System.out.print("Enter an adjective (description): ");
+            adjective2 = scanner.nextLine();
+
+            System.out.print("Enter an verb finish by ing: ");
+            verb1 = scanner.nextLine();
+
+            System.out.print("Enter the adjective: ");
+            adjective3 = scanner.nextLine();
+
+
+
 
             System.out.println("--- Summary ---");
-            System.out.println("the width is :"+width);
-            System.out.println("the hieght is :"+height);
-            System.out.println("the area is :"+area+"cm^2");
+            System.out.println("Today i want to a :" +adjective1+ "zoo");
+            System.out.println("In an exhibit, I saw a :"+noun1+".");
+            System.out.println(noun1+"was :"+adjective2+"and"+verb1+"!");
+            System.out.println("I was :"+ adjective3 +"!");
+
 
         } catch (NumberFormatException e) {
             System.out.println("Error: Please enter valid numbers!");
