375ed8932fdde7357e0346b646f30d1ced555c56
ilyass <ilyassselarabi1999@gmail.com>
2025-10-14 14:17:18 +0100

    random numbers

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,24 @@
 import java.util.Scanner;
+import java.util.Random ;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
 
         try {
-            System.out.print("Entrez votre âge : ");
-            int age = sc.nextInt();
+            int number ;
+            boolean isHeads;
+            number = rand.nextInt(77,7766)+1 ;
+            isHeads=rand.nextBoolean() ;
 
-            // Condition if-else
-            if (age >= 18) {
-                System.out.println("Vous êtes majeur");
-            } else {
-                System.out.println("Vous êtes mineur");
+            if(isHeads){
+                System.out.println("is true");
             }
+            else{
+                System.out.println("is false");
+            }
+            System.out.println(number);
 
         } catch (NumberFormatException e) {
             System.out.println(" Error: Please enter valid numbers!");
