f9bcbff0ff9652748b30f525f7e1780dde747a31
ilyass <ilyassselarabi1999@gmail.com>
2025-10-15 10:39:17 +0100

    string methods Examples

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 25;
-        double income = 45000;
-        boolean hasGoodCredit = true;
 
-        if (age >= 21) {
-            if (income >= 30000) {
-                if (hasGoodCredit) {
-                    System.out.println("Loan Approved!");
-                } else {
-                    System.out.println("Loan Denied: Poor credit history");
-                }
-            } else {
-                System.out.println("Loan Denied: Insufficient income");
-            }
-        } else {
-            System.out.println("Loan Denied: Below minimum age requirement");
-        }
+
+        String name="BroCode ";
+
+
+        int len = name.length();
+        char letter = name.charAt(0);
+        int index = name.indexOf("");
+        int Lastindex = name.lastIndexOf("o");
+
+//          name=name.toLowerCase();
+//          name=name.toUpperCase();
+//          name=name.trim();
+            name=name.replace("o","d");
+
+
+        System.out.println("Last Index: " + Lastindex);
+        System.out.println("len: " + len);
+        System.out.println(" Index: " + index);
+        System.out.println("letter: " + letter);
+        System.out.println("Name: " + name);
+        System.out.println(name.isEmpty()?"salut":"Bonjour");
+        System.out.println(name.contains("d")?"salut":"Bonjour");
+
     }
 }
 
