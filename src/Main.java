7574b0aab10b0e727d22f6dcd302e1c9d9ae2f57
ilyass <ilyassselarabi1999@gmail.com>
2025-10-15 10:54:01 +0100

    substrings method

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,15 @@ public class Main {
     public static void main(String[] args) {
 
 
-        String name="BroCode ";
+        String email="ilyasssilarabi1999@gmail.com ";
+        String username=email.substring(2,email.indexOf("@"));
+        String test=email.substring(email.indexOf("@")-1);
+        System.out.println(username);
+        System.out.println(test);
 
 
-        int len = name.length();
-        char letter = name.charAt(0);
-        int index = name.indexOf("");
-        int Lastindex = name.lastIndexOf("o");
-
-//          name=name.toLowerCase();
-//          name=name.toUpperCase();
-//          name=name.trim();
-            name=name.replace("o","d");
 
 
-        System.out.println("Last Index: " + Lastindex);
-        System.out.println("len: " + len);
-        System.out.println(" Index: " + index);
-        System.out.println("letter: " + letter);
-        System.out.println("Name: " + name);
-        System.out.println(name.isEmpty()?"salut":"Bonjour");
-        System.out.println(name.contains("d")?"salut":"Bonjour");
 
     }
 }
