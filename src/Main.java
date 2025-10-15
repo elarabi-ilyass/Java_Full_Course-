dce380406cac242e5e2465330b77ee9aa7e13ba3
ilyass <ilyassselarabi1999@gmail.com>
2025-10-15 12:12:53 +0100

    weight converter Example Tp

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,47 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+//        double numConvert;
+        int choose;
+        int weight;
+        double total;
+
+      try{
+          System.out.println("Please enter the number to convert it if you want: ");
+          System.out.println("1 : Convert lbs to kg");
+          System.out.println("2 : Convert kg to lbs");
+          choose = sc.nextInt();
+          switch(choose){
+              case 1:
+                  System.out.println("Please enter weight value: ");
+                  weight=sc.nextInt();
+                  total= weight* 0.453592;
+                  System.out.printf("The new weight is: %.2f%n  ",total);
+                  break;
+
+              case 2:
+                  System.out.println("Please enter weight value: ");
+                  weight=sc.nextInt();
+                  total= weight*2.20462262;
+                  System.out.printf("The new number is: %.2f%n  ",total);
+                  break;
+
+                default:
+                    System.out.println("Invalid choice");
+          }
 
 
-        String email="ilyasssilarabi1999@gmail.com ";
-        String username=email.substring(2,email.indexOf("@"));
-        String test=email.substring(email.indexOf("@")-1);
-        System.out.println(username);
-        System.out.println(test);
 
+      }
+      catch(Exception e){
+          System.out.println(e);
+      }
+      finally {
+            sc.close();
+      }
 
 
 
