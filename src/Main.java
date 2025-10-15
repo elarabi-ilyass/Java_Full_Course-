b7e6dd631197d9ca0afff91eee05e5db9ee0c494
ilyass <ilyassselarabi1999@gmail.com>
2025-10-15 09:49:27 +0100

    Example of nested if statements

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,24 @@
-import java.util.Scanner;
-import java.util.InputMismatchException; // Import ajouté
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        int age = 25;
+        double income = 45000;
+        boolean hasGoodCredit = true;
 
-
-        double principale;
-        double rate;
-        double timesCompounded;
-        double years;
-        double amount;
-
-
-        try {
-            System.out.println(" Enter the principale amount");
-            principale=sc.nextDouble();
-            System.out.println(" Enter the rate");
-            rate=sc.nextDouble() / 100;
-            System.out.println(" Enter the timesCompounded");
-            timesCompounded=sc.nextDouble();
-            System.out.println(" Enter the years");
-            years=sc.nextDouble();
-
-            amount=principale * Math.pow(1+rate / timesCompounded,timesCompounded * years);
-
-            System.out.println("Total amount is  " +amount);
-        } catch (InputMismatchException e) { // Exception corrigée
-            System.out.println(" Error: Please enter valid numbers!");
-        } finally {
-            sc.close();
+        if (age >= 21) {
+            if (income >= 30000) {
+                if (hasGoodCredit) {
+                    System.out.println("Loan Approved!");
+                } else {
+                    System.out.println("Loan Denied: Poor credit history");
+                }
+            } else {
+                System.out.println("Loan Denied: Insufficient income");
+            }
+        } else {
+            System.out.println("Loan Denied: Below minimum age requirement");
         }
     }
 }
-
-
-
 
 
 
